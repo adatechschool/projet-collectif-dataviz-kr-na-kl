dca190f800b2e6beb1255407c4540b0dcfcba797
Klervy <112544312+KatShulu@users.noreply.github.com>
2022-11-29 15:33:45 +0100

    premiere etape js, fetch+createUrl

--- src/dataViz.js
+++ src/dataViz.js
@@ -1,9 +1,27 @@
-let number = 50 
+/*let number = 50 
 
 let allier = document.getElementById('FR-04');
 
 if (number === 50) {
     allier.style.fill ='red'
 
+}*/
+
+
+function fetchPos(link){
+    fetch(link)
+    .then(response=>{
+        return response.json()
+    })
+    .then(data=>{
+        return console.log(data[0].pos)
+    })
 }
 
+function createLinkFetch(depname,date){
+    let link= String("https://coronavirusapifr.herokuapp.com/data/departement/"+depname+"/"+date)
+    fetchPos(link)
+}
+
+
+createLinkFetch("rhone","10-11-2021")
