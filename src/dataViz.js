cdc82156d2185da7084977cfb5fe121d78744fc9
nadhell <nadhell@live.com>
2022-12-01 14:11:27 +0100

    changed the if/else function returning the color with a switch/case function

--- src/dataViz.js
+++ src/dataViz.js
@@ -1,5 +1,5 @@
 
-// returns a Promise and with Json we trassform the date into reusable data .
+// returns a Promise and with Json we transform the date into reusable data .
 // In case of error we error we catch the error 
 
 function getAllDepartments(date) {
@@ -16,23 +16,36 @@ function getAllDepartments(date) {
 // line 32 gets the each department numbers 
 // line 35 gets each departments numbers from html documet it is linked to line 32
 // line 36 fills the colors according the hospitalisation cases 
-
-getAllDepartments('09-07-2021').then(departments => {
-    for(departement of departments) {
-        let elementId = `FR-${departement.dep}`;
+/*
+getAllDepartments('10-04-2021').then(departments => {
+    for(department of departments) {
+        let elementId = `FR-${department.dep}`;
         console.log(elementId)
         let depHtml = document.getElementById(elementId);
         if(depHtml) {
-            depHtml.style.fill = covidColors(departement.hosp);
+            depHtml.style.fill = covidColors(department.hosp);
         }
     }
 })
+*/
+
+getAllDepartments('10-04-2021').then(departments => {
+    departments.forEach(department => {//console.log(department)
+        let elementId = `FR-${department.dep}`;
+        console.log(elementId)
+        let depHtml = document.getElementById(elementId);
+        if(depHtml) {
+            depHtml.style.fill = covidColors(department.hosp);
+        }
+    })
+    
+})
 
 // this function gives the different colors according to the number of the hospitalisation
-
+/*
 function covidColors (numberOfCases) {
     console.log(numberOfCases)
-    let color = '#007f5f'
+    let color = '#CECECE'
     if (numberOfCases < 50) {
 
         color = '#007f5f'
@@ -47,7 +60,7 @@ function covidColors (numberOfCases) {
         color = '#aacc00'
     }
     else if (numberOfCases < 250) {
-        color = '#2d4d700'
+        color = '#2d4d70'
     }
     else if (numberOfCases < 300) {
         color = '#eeef20'
@@ -67,8 +80,42 @@ function covidColors (numberOfCases) {
     else if (numberOfCases < 700) {
         color = '#9d0208'
     }
-    else if (numberOfCases < 800) {
-        color = '6a040f'
+    else {
+        color = '#6a040f'
+
     }
     return color
+}
+*/
+
+function covidColors (numberOfCases) {
+    console.log(numberOfCases)
+    switch(numberOfCases){
+        case (numberOfCases>0 && numberOfCases<=50): color = '#007f5f';
+            break;
+        case (numberOfCases>50 && numberOfCases<=100): color = '#2b9348';
+            break;
+        case (numberOfCases>100 && numberOfCases<=150): color = '#80b918';
+            break;
+        case (numberOfCases>150 && numberOfCases<=200): color = '#aacc00';
+            break;
+        case (numberOfCases>200 && numberOfCases<=250): color = '#2d4d70';
+            break;
+        case (numberOfCases>250 && numberOfCases<=300): color = '#eeef20';
+            break;        
+        case (numberOfCases>300 && numberOfCases<=350): color = '#ffba08';
+            break;
+        case (numberOfCases>350 && numberOfCases<=400): color = '#faa307';
+            break;
+        case (numberOfCases>400 && numberOfCases<=500): color = '#e85d04';
+            break;
+        case (numberOfCases>500 && numberOfCases<=600): color = '#dc2f02';
+            break;
+        case (numberOfCases>600 && numberOfCases<=700): color = '#9d0208';
+            break;
+        case (numberOfCases)>700: color = '#9d0208';
+            break;
+        default: color = '#CECECE';
+   }
+   return color;
 }
