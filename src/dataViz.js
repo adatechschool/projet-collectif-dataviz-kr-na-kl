bdf7bcfef8b25c30b058fefe1fd7cfca2e3ab1bb
Kristine Kirakosiani <kristine.kirakossiani@gmail.com>
2022-12-05 16:23:03 +0100

    html page update, legend added.

--- src/dataViz.js
+++ src/dataViz.js
@@ -35,14 +35,14 @@ getAllDepartments('10-04-2021').then(departments => {
         console.log(elementId)
         let depHtml = document.getElementById(elementId);
         if(depHtml) {
-            depHtml.style.fill = covidColors(department.hosp);
+            depHtml.style.fill = covidColors(department.pos);
         }
     })
     
 })
 
 // this function gives the different colors according to the number of the hospitalisation
-/*
+
 function covidColors (numberOfCases) {
     console.log(numberOfCases)
     let color = '#CECECE'
@@ -86,36 +86,36 @@ function covidColors (numberOfCases) {
     }
     return color
 }
-*/
 
-function covidColors (numberOfCases) {
-    console.log(numberOfCases)
-    switch(numberOfCases){
-        case (numberOfCases>0 && numberOfCases<=50): color = '#007f5f';
-            break;
-        case (numberOfCases>50 && numberOfCases<=100): color = '#2b9348';
-            break;
-        case (numberOfCases>100 && numberOfCases<=150): color = '#80b918';
-            break;
-        case (numberOfCases>150 && numberOfCases<=200): color = '#aacc00';
-            break;
-        case (numberOfCases>200 && numberOfCases<=250): color = '#2d4d70';
-            break;
-        case (numberOfCases>250 && numberOfCases<=300): color = '#eeef20';
-            break;        
-        case (numberOfCases>300 && numberOfCases<=350): color = '#ffba08';
-            break;
-        case (numberOfCases>350 && numberOfCases<=400): color = '#faa307';
-            break;
-        case (numberOfCases>400 && numberOfCases<=500): color = '#e85d04';
-            break;
-        case (numberOfCases>500 && numberOfCases<=600): color = '#dc2f02';
-            break;
-        case (numberOfCases>600 && numberOfCases<=700): color = '#9d0208';
-            break;
-        case (numberOfCases)>700: color = '#9d0208';
-            break;
-        default: color = '#CECECE';
-   }
-   return color;
-}
+
+// function covidColors (numberOfCases) {
+//     console.log(numberOfCases)
+//     switch(numberOfCases){
+//         case 85: color = '#007f5f';
+//             break;
+//         case (numberOfCases>50 && numberOfCases<=100): color = '#2b9348';
+//             break;
+//         case (numberOfCases>100 && numberOfCases<=150): color = '#80b918';
+//             break;
+//         case (numberOfCases>150 && numberOfCases<=200): color = '#aacc00';
+//             break;
+//         case (numberOfCases>200 && numberOfCases<=250): color = '#2d4d70';
+//             break;
+//         case (numberOfCases>250 && numberOfCases<=300): color = '#eeef20';
+//             break;        
+//         case (numberOfCases>300 && numberOfCases<=350): color = '#ffba08';
+//             break;
+//         case (numberOfCases>350 && numberOfCases<=400): color = '#faa307';
+//             break;
+//         case (numberOfCases>400 && numberOfCases<=500): color = '#e85d04';
+//             break;
+//         case (numberOfCases>500 && numberOfCases<=600): color = '#dc2f02';
+//             break;
+//         case (numberOfCases>600 && numberOfCases<=700): color = '#9d0208';
+//             break;
+//         case (numberOfCases)>700: color = '#9d0208';
+//             break;
+//         default: color = '#CECECE';
+//    }
+//    return color;
+// }
