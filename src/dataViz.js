2d6e537eb715f4aa7e875fb1e7537224ac946d0b
Klervy <112544312+KatShulu@users.noreply.github.com>
2022-12-06 10:42:53 +0100

    add toolpic

--- src/dataViz.js
+++ src/dataViz.js
@@ -42,7 +42,7 @@ getAllDepartments('10-04-2021').then(departments => {
 })
 
 // this function gives the different colors according to the number of the hospitalisation
-/*
+
 function covidColors (numberOfCases) {
     console.log(numberOfCases)
     let color = '#CECECE'
@@ -86,7 +86,7 @@ function covidColors (numberOfCases) {
     }
     return color
 }
-*/
+/*
 
 function covidColors (numberOfCases) {
     console.log(numberOfCases)
@@ -119,3 +119,53 @@ function covidColors (numberOfCases) {
    }
    return color;
 }
+*/
+function toggleDataCreation() {
+    //This function allow to reuse the result of the fetch and create an array of objects with the departments and positive cases
+    toggleData = new Array();                                     //Creation of the array
+    getAllDepartments("10-04-2021").then((departments) => {       //When the fetch is available do
+      departments.forEach((department) => {                       //For each object of the response do
+        obj = new Object();                                       //Creation of the obj
+        Object.assign(obj, { department: department.lib_dep });   //Push the name with the key departement
+        Object.assign(obj, { pos: department.pos });              //Push the positives cases num with the key pos
+        toggleData.push(obj);                                     //Push the obj into the array
+      });
+    });
+    return toggleData;
+  }
+  
+  function SVGToolTip() {
+    //This function handle every thing that relate too the tooltip(info-bulle)
+    //scr :https://nosmoking.developpez.com/demos/rep-forum/d2072572.html
+    const oSVG = document.querySelector("svg");                   //Create a function that contain all the html svg
+    const oInfo = document.createElement("DIV");                  //Create a div in the html
+    oInfo.id = "info-SVG";                                        //create a infraclass info-SVG
+    oSVG.parentNode.append(oInfo);                                //Not sure about this one
+    toggleDataCreation();                                         //Call for the data needed for the tooltip
+  
+    function _toggleInfo(e) {
+      //This function handle the text that must be written in the tooltip
+      for (i = 0; i < toggleData.length; i++) {                   //Go trough the array to make connection between the svg and the pos data
+        if (toggleData[i].department == e.target.dataset.title) { //If the name in a array match the title of the svg in html
+          oInfo.innerHTML =                                       //The text of the toolpic =
+            "mouseenter" === e.type ? "<h1>" + toggleData[i].department +"<br>"+ toggleData[i].pos + " cas positif </h1>" : "";
+            //Not my syntax but if the event type === mouseenter do show dep name and positives cases
+        }
+      }
+    }
+  
+    function _moveInfo(e) {
+      //This function tracs the position of the mouse, and replicate the movement onto css
+      oInfo.style.transform =                                   //Change trough the css
+        "translate3d(" + e.layerX + "px," + e.layerY + "px, 0)";//Tans3D is a proprety of css
+    }
+  
+    oSVG.addEventListener("mousemove", _moveInfo, true);        // Check if the mouse is on the svg and call moveInfo
+    const oElems = document.querySelectorAll("path");           //Check if mouse is on PATH
+    oElems.forEach(function (elem) {                            //For each time the mouse moove check if toggle info is true
+      elem.addEventListener("mouseenter", _toggleInfo);
+      elem.addEventListener("mouseleave", _toggleInfo);
+    });
+  }
+  SVGToolTip();                                                 //Call of the function
+  
