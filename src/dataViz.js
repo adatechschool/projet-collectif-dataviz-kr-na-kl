5451d846a0ce64051cd29e24db4d6cf249c34b74
Klervy <112544312+KatShulu@users.noreply.github.com>
2022-12-06 18:08:29 +0100

    fix toggle problem

--- src/dataViz.js
+++ src/dataViz.js
@@ -8,10 +8,6 @@ function getValue() {
   return inputDate;
 }
 
-button.onclick = function () {
-  let myNewDate = getValue();
-  fetchValuesWith(myNewDate);
-};
 // Returns a Promise, Json transforms the result in reusable data
 // Then it catches errors
 function getAllDepartments(date) {
@@ -30,9 +26,9 @@ function getAllDepartments(date) {
 function fetchValuesWith(date) {
   getAllDepartments(date).then((departments) => {
     departments.forEach((department) => {
-      console.log(department);
+      //console.log(department);
       let elementId = `FR-${department.dep}`;
-      console.log(elementId);
+      //console.log(elementId);
       let depHtml = document.getElementById(elementId);
       if (depHtml) {
         depHtml.style.fill = covidColors(department.pos);
@@ -43,7 +39,7 @@ function fetchValuesWith(date) {
 
 // This function attributes a color code depending on the number of covid cases :
 function covidColors(numberOfCases) {
-  console.log(numberOfCases);
+  //console.log(numberOfCases);
   let color = "#CECECE";
   if (numberOfCases < 50) {
     color = "#007f5f";
@@ -75,41 +71,45 @@ function covidColors(numberOfCases) {
   return color;
 }
 
-function toggleDataCreation() {
+function toggleDataCreation(date) {
   //This function allow to reuse the result of the fetch and create an array of objects with the departments and positive cases
-  toggleData = new Array();                                   //Creation of the array
-  getAllDepartments("10-04-2021").then((departments) => {     //When the fetch is available do
+  toggleData = new Array();                                     //Creation of the array
+  getAllDepartments(date).then((departments) => {               //When the fetch is available do
+    
 
-    departments.forEach((department) => {                     //For each object of the response do
+    departments.forEach((department) => {                       //For each object of the response do
 
-      obj = new Object();                                     //Creation of the obj
-      Object.assign(obj, { department: department.lib_dep }); //Push the name with the key departement
-      Object.assign(obj, { pos: department.pos });            //Push the positives cases num with the key pos
-      toggleData.push(obj);                                   //Push the obj into the array
+
+      obj = new Object();                                       //Creation of the obj
+      Object.assign(obj, { department: department.lib_dep });   //Push the name with the key departement
+      Object.assign(obj, { pos: department.pos });              //Push the positives cases num with the key pos
+      toggleData.push(obj);                                     //Push the obj into the array
     });
   });
   return toggleData;
 }
 
-function SVGToolTip() {
+function SVGToolTip(date) {
   //This function handle every thing that relate too the tooltip(info-bulle)
   //scr :https://nosmoking.developpez.com/demos/rep-forum/d2072572.html
-  const oSVG = document.querySelector("svg");                  //Create a function that contain all the html svg
-  const oInfo = document.createElement("DIV");                 //Create a div in the html
-  oInfo.id = "info-SVG";                                       //create a infraclass info-SVG
-  oSVG.parentNode.append(oInfo);                               //Not sure about this one
-  toggleDataCreation();                                        //Call for the data needed for the tooltip
+  const oSVG = document.querySelector("svg");                   //Create a function that contain all the html svg
+  const oInfo = document.createElement("DIV");                  //Create a div in the html
+  oInfo.id = "info-SVG";                                        //create a infraclass info-SVG
+  oSVG.parentNode.append(oInfo);                                //Not sure about this one
+  toggleDataCreation(date);                                     //Call for the data needed for the tooltip
+
 
   function _toggleInfo(e) {
     //This function handle the text that must be written in the tooltip
-    for (i = 0; i < toggleData.length; i++) {                  //Go trough the array to make connection between the svg and the pos data
+    for (i = 0; i < toggleData.length; i++) {                   //Go trough the array to make connection between the svg and the pos data
+
 
       if (toggleData[i].department == e.target.dataset.title) {//If the name in a array match the title of the svg in html
 
-        oInfo.innerHTML =                                      //The text of the toolpic =
-          "mouseenter" === e.type ?                            //Not my syntax but if the event type === mouseenter do show dep name and positives cases
 
-             "<h1>" +
+        oInfo.innerHTML =                                      //The text of the toolpic =
+          "mouseenter" === e.type                              //Not my syntax but if the event type === mouseenter do show dep name and positives cases
+            ? "<h1>" +
               toggleData[i].department +
               "<br>" +
               toggleData[i].pos +
@@ -133,4 +133,9 @@ function SVGToolTip() {
     elem.addEventListener("mouseleave", _toggleInfo);
   });
 }
-SVGToolTip();                                                   //Call of the function
+
+button.onclick = function () {
+  let myNewDate = getValue();
+  fetchValuesWith(myNewDate);
+  SVGToolTip(myNewDate);
+};
